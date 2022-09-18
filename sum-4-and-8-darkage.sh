84a31eca17f741a4a3e814ee030b06ded8414ab8
Emir Dzakic <miro.dzakic@gmail.com>
2022-09-18 21:50:26 +0200

    Update sum-4-and-8-darkage.sh

--- sum-4-and-8-darkage.sh
+++ sum-4-and-8-darkage.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Write sum-4-and-8-darkage.sh, to find the sum of the numbers between 500 and 1500 
-#(. dark ages) having digits only from the set {4, 8} on the second place. e.g  549, 580..1492..1498
+#(. dark ages) having digits only from the set {4, 8} on the second place. e.g  549, 580..940..941..981..9821492..1498
 # Hint: Use a brace expansion to make the range of numbers, a loop to check each one, and a
-# conditional statement including a regular expression to see whether the digits are in {4, 8}).
+# conditional statement including a regular expression to see whether the second digit is in {4, 8}).
 
 # result line is "sum of all years in dark ages including 4 and 8 on the second place is: 221400"
 
